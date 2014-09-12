0aa3d8f4ab90a50738978a5002ba7d4e99e592ae
Ryan Partlan <ryanstartedit@gmail.com>
2014-09-12 16:14:45 -0700

    changes to newcar.py
    Included a feature to allow the user to visit the site by typing “yes”.
    This can be useful if the user sees a price they like.

--- newcar.py
+++ newcar.py
@@ -1,5 +1,6 @@
 import bs4
 import requests
+import webbrowser
 
 make = raw_input("Enter the make: ")
 model = raw_input("Enter the model: ")
@@ -10,8 +11,17 @@ zipcode = raw_input("Enter your zip code: ")
 city = cityname.replace(' ','+')
 carmodel = model.replace(' ','+')
 
-r = requests.get("http://www.autotrader.com/cars-for-sale/New+Cars/"+make+"/"+carmodel+"/"+city+"+"+state+"-"+zipcode+"?endYear=2015&listingType=new&listingTypes=new&searchRadius=100")
+url = "http://www.autotrader.com/cars-for-sale/New+Cars/"+make+"/"+carmodel+"/"+city+"+"+state+"-"+zipcode+"?endYear=2015&listingType=new&listingTypes=new&searchRadius=100"
+
+r = requests.get(url)
 soup = bs4.BeautifulSoup(r.text)
 priceRange = soup.html.body.findAll('ul',{'class':'price-range-list'})
 for i in priceRange:
     print(i.text)
+
+site = raw_input("Would you like to go to the site? ")
+lowersite = site.lower()
+if lowersite == "yes" or lowersite == "y":
+    webbrowser.open_new(url)
+else:
+    print "Goodbye"
