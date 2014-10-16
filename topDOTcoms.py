2775f03a9e51df2fde4b0dfc08ef9708a1ed7df2
Ryan Partlan <ryanstartedit@gmail.com>
2014-10-15 17:33:26 -0700

    topDotComs.py
    added error handling for failed requests.

--- topDOTcoms.py
+++ topDOTcoms.py
@@ -2,17 +2,21 @@ import requests
 import BeautifulSoup
 
 domain = raw_input("Enter a domain: ")
-
+print("")
 
 r = requests.get('https://domaintyper.com/top-websites/most-popular-websites-with-'+domain+'-domain')
-soup = BeautifulSoup.BeautifulSoup(r.text)
-tops = soup.html.body.findAll('tr')
-counter = 0
-for i in tops:
-    counter+=1
-    rankings = str(i.text)
-    result = rankings.replace(domain,domain+" ")
-    print(result)
-    if counter>10:
-        break
-        
+
+if r.status_code == 200:
+    soup = BeautifulSoup.BeautifulSoup(r.text)
+    tops = soup.html.body.findAll('tr')
+    counter = 0
+    for i in tops:
+        counter+=1
+        rankings = str(i.text)
+        result = rankings.replace(domain,domain+" ")
+        print(result)
+        if counter>10:
+            break
+
+else:
+    print("There are no domains by that name.")
